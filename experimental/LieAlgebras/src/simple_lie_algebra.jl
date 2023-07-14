0ebe84da6df3d37c3dc05d63b8956bdb685793e8
voggesbe <101730185+voggesbe@users.noreply.github.com>
2023-07-14 14:43:53 +0200

    Update simple_lie_algebra.jl

--- experimental/LieAlgebras/src/simple_lie_algebra.jl
+++ experimental/LieAlgebras/src/simple_lie_algebra.jl
@@ -94,6 +94,9 @@ function Base.show(io::IO, L::SimpleLieAlgebra)
     )
     print(IOContext(io, :supercompact => true), coefficient_ring(L))
   end
+
+function symbols(L::SimpleLieAlgebra) 
+  return L.s
 end
 
 ###############################################################################
